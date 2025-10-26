6ceb958510588b1aa5a204bfdbb9fa381e65beae
MiguelCastilloSanchez <A18003592@alumnos.uady.mx>
2025-10-26 08:21:05 -0600

    feat: main files

---
+++ src/test/java/com/cupersonal/app_api/AppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cupersonal.app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
